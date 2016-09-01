25eb56aaf1ab3b1f6a68d489a832e09d09057240
Jonathan Piat <piat.jonathan@gmail.com>
2016-09-01 09:38:41 +0000

    Now control ESC and steering servo

--- servo_control.c
+++ servo_control.c
@@ -5,8 +5,17 @@
 
 #include <pigpio.h>
 
-#define SERVO_1 12
-#define SERVO_2 13
+#define ESC 12
+#define SERVO 13
+
+#define MIN_ESC 800
+#define CENTER_ESC 1500
+#define MAX_ESC  1600
+#define ARM_ESC 1500
+
+#define MIN_SERVO 1000
+#define CENTER_SERVO 1500
+#define MAX_SERVO 2000
 
 int main(void){
 	int i  ;
@@ -16,10 +25,19 @@ int main(void){
 	exit(-1);
 	 // pigpio initialisation failed.
 	}
-	for(i = 500 ; i < 2500 ; i ++){
-		gpioServo(SERVO_1, i);
-		sleep(100);
+	gpioServo(ESC, ARM_ESC);
+	printf("Arming ESC \n");
+	sleep(2);
+	for(i = MIN_SERVO ; i < MAX_SERVO ; i +=10){
+		gpioServo(SERVO, i);
+		usleep(50000);
 	}
+	/*
+	for(i = MIN_ESC ; i < MAX_ESC ; i +=10){
+                gpioServo(ESC, i);
+                usleep(50000);
+        }*/
+
 	gpioTerminate();
 	return 1 ;
 }
