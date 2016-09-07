60d9970f28856b0861d64fbca60bc507d9174b6a
Jonathan Piat <piat.jonathan@gmail.com>
2016-09-07 23:50:30 +0200

    Added include file for servo control

---
+++ inc/servo_control.h
@@ -0,0 +1,29 @@
+#ifdef __arm__
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdarg.h>
+#include <unistd.h>
+
+#include <pigpio.h>
+
+#ifndef SERVO_CONTROL_H
+#define SERVO_CONTROL_H
+
+#define ESC 12
+#define SERVO 13
+
+#define MIN_ESC 800
+#define CENTER_ESC 1500
+#define MAX_ESC  1600
+#define ARM_ESC 1500
+
+#define MIN_SERVO 1000
+#define CENTER_SERVO 1500
+#define MAX_SERVO 2000
+
+void arm_esc();
+void set_esc_speed(float speed);
+void set_servo_angle(float angle);
+int test_servo(void);
+#endif
+#endif
