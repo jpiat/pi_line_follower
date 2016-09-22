e2355c8caa55578a6c926ba242691dc7ccd1ee8b
Jonathan Piat <piat.jonathan@gmail.com>
2016-09-22 12:48:09 +0000

    Corrected bug in servo control

--- inc/servo_control.h
+++ inc/servo_control.h
@@ -13,11 +13,11 @@
 
 #define MIN_ESC 800
 #define CENTER_ESC 1500
-#define MAX_ESC  1600
-#define ARM_ESC 1500
-
-#define MIN_SERVO 1200
-#define CENTER_SERVO 1500
+#define MAX_ESC  1700
+#define ARM_ESC_1 800
+#define ARM_ESC_2 1500
+#define MIN_SERVO 1100
+#define CENTER_SERVO 1400
 #define MAX_SERVO 1800
 
 #ifndef SERVO_CONTROL_H
