e7a1028a9851539a5d6fb902be3036060bb47871
Jonathan Piat <piat.jonathan@gmail.com>
2016-09-27 21:17:32 +0200

    Added sync command to make sure the log made it to the disk.

--- launch_race.sh
+++ launch_race.sh
@@ -5,7 +5,8 @@ touch tests/polypheme.cpp
 make polypheme MODE=-DRUN
 sync
 read -p "Make sure emergency stop is dis-engaged and press key" -n1 -s
-echo "Press switch to initiate start and release to start countdown"
+echo "Press switch to initiate start and release to start 2s countdown"
 echo "Disconnect usb serial interface"
 sudo ./polypheme
+sync
 echo "Engage emergency stop"
